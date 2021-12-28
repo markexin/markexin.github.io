97309a0b59366a95da1a9a4b0cd9765a17aadedb
maqun <maqun1@xiaomi.com>
2021-12-28 11:27:24 +0800

    .

--- node_modules/.pnpm/hexo-renderer-sass@0.4.0_hexo@6.0.0/node_modules/hexo-renderer-sass/index.js
+++
@@ -1,8 +0,0 @@
-/* global hexo */
-'use strict'
-
-var sassRenderer = require('./lib/renderer')
-
-// associate the Sass renderer with .scss AND .sass extensions
-hexo.extend.renderer.register('scss', 'css', sassRenderer('scss'))
-hexo.extend.renderer.register('sass', 'css', sassRenderer('sass'))
